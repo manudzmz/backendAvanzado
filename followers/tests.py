67112cb89618d7f788881c27b6db631e43037e0d
Manuel <manudzmz@gmail.com>
2016-10-18 18:02:46 +0200

    Creados dos test que funcionan.

--- followers/tests.py
+++ followers/tests.py
@@ -3,16 +3,22 @@ from django.test import TestCase
 
 # Create your tests here.
 from followers.models import Relationship
-from followers.utils import get_followers
+from followers.utils import get_followers, get_following
 
 
 class RelationshipTests(TestCase):
+    def setUp(self):
+        """
+        Se ejecuta antes de cada test
+        """
+        self.user1 = User.objects.create_user('luke', 'skywalker@starwars.com', "skywalker")
+        self.user2 = User.objects.create_user('anakin', 'annie@starwars.com', 'skywalker')
+        Relationship.objects.create(origin=self.user1, target=self.user2)  # user1 sigue a user2
+
     def test_get_followers_returns_users_that_follow_a_given_user(self):
-        user1 = User.objects.create_user('luke', 'skywalker@starwars.com', "skywalker")
-        user2 = User.objects.create_user('anakin', 'annie@starwars.com', 'skywalker')
+        followers = get_followers(self.user2)
+        self.assertEqual([self.user1], followers)  # followers == [user1]
 
-        Relationship.objects.create(origin=user1, target=user2)  # user1 sigue a user2
-
-        followers = get_followers(user2)
-
-        self.assertEqual([user1], followers)  # followers == [user1]
+    def test_get_following_returns_users_followed_by_a_given_user(self):
+        following = get_following(self.user1)
+        self.assertEqual([self.user2], following)  # following == [user2]
--- followers/utils.py
+++ followers/utils.py
@@ -6,5 +6,14 @@ def get_followers(user):
     followers_pks = user.relationship_target.all().values_list('origin__pk', flat=True)
     return list(User.objects.filter(pk__in=followers_pks))
 
+    # Metodo 2
+    # relationships = Relationship.objects.filter(target=user).select_related('origin')
+    # followers = list()
+    # for relationship in relationships:
+    #     followers.append(relationship.origin)
+    # return followers
+
+
 def get_following(user):
-    pass
+    following_pks = user.relationship_origin.all().values_list('target__pk', flat=True)
+    return list(User.objects.filter(pk__in=following_pks))
