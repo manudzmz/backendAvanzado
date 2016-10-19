3231e4457071cde21c5be5922e8af83b025b7ecc
Manuel <manudzmz@gmail.com>
2016-10-19 13:19:51 +0200

    Refactorizado el ViewSet y separados los tests en varios archivos.

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,4 @@
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -6,11 +7,15 @@ from followers.serializers import RelationshipUserSerializer
 from followers.utils import get_following
 
 
-class FollowingViewSet(GenericViewSet):
+class FollowingViewSet(ListModelMixin, GenericViewSet):
     serializer_class = RelationshipUserSerializer
     permission_classes = (IsAuthenticated,)
 
-    def list(self, request):
-        following = get_following(request.user)
-        serializer = RelationshipUserSerializer(following, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return get_following(self.request.user)
+
+    # Al añadir herencia de ListModelMixin no hace falta este parrafo
+    # def list(self, request):
+    #     following = get_following(request.user)
+    #     serializer = RelationshipUserSerializer(following, many=True)
+    #     return Response(serializer.data)
